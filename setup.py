7b99a9254cc5794038cc3f0ed349a589fe61a6d2
Serhiy Stetskovych <patriotyk@gmail.com>
2024-10-21 17:08:07 +0300

    Update version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 version = '1.3.dev{build_time}'.format(build_time=int(time.time()))
 
 requires = [
-    'Django<=4.2',
+    'Django<=4.2.15',
     'ipaddress',
     'six',
 ]
