b51ec6ebab1bc6f70a0f44584b64a73dd899892a
Serhiy Stetskovych <patriotyk@gmail.com>
2024-11-05 18:26:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-version = '1.3.dev{build_time}'.format(build_time=int(time.time()))
+version = '1.4.0'
 
 requires = [
     'Django<=4.2.15',
